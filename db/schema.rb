e92e40b133be5091290b6f1aa961871b91a290ee
Said Maadan <me@saidmaadan.com>
2017-10-17 19:48:37 -0500

    Created exercise model and association with user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015005233) do
+ActiveRecord::Schema.define(version: 20171018004525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: :cascade do |t|
+    t.integer  "duration_in_min"
+    t.text     "workout"
+    t.date     "workout_date"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_exercises_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 20171015005233) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "exercises", "users"
 end
